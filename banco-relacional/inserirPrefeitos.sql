a5571a6517d9c80a90d984c9a5835e97f94b7d88
gabriel7mendes <andrewsseven@gmail.com>
2021-08-25 15:21:52 -0300

    301. Inserir Prefeitos

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,20 @@
+select * from cidades;
+
+INSERT INTO prefeitos
+  (nome, cidade_id)
+VALUES
+   ('Rodrigo Neves', 7),
+   ('Raquel Lyra', 8),
+   ('Zenaldo Coutinho', null);
+
+select * from prefeitos;
+
+INSERT INTO prefeitos
+  (nome, cidade_id)
+VALUES 
+   ( 'Rafael Greca', null);
+
+INSERT INTO prefeitos
+  (nome, cidade_id)
+VALUES 
+   ( 'Rodrigo Pinheiro', null);
