d6aad0bdf536353ea358b6d3fd3bd9c01b7d47fd
gabriel7mendes <andrewsseven@gmail.com>
2022-03-01 12:31:33 -0300

    440. Consulta de Produto por Nome

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -36,6 +36,12 @@ public class ProdutoController {
 	public Iterable<Produto> obterProdutos() {
 		return produtoRepository.findAll();
 	}
+	
+	@GetMapping(path = "/nome/{parteNome}")
+	public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome) {
+		return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+	}
+
 
 	@GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
 	public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina) {
--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import br.com.cod3r.exerciciossb.model.entities.Produto;
 
 public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
-
+	
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
+	
 }
