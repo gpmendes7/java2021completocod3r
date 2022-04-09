d96bdd433ef4fc9d72a528b7146c85af2650a687
gabriel7mendes <andrewsseven@gmail.com>
2022-04-09 16:06:34 -0300

    180. Consumer

--- exercicios/src/lambdas/Produto.java
+++ exercicios/src/lambdas/Produto.java
@@ -12,6 +12,9 @@ public class Produto {
 		this.desconto = desconto;
 	}
 	
-	
+	public String toString() {
+		double precoFinal = preco * (1 - desconto);
+		return nome + " tem preço de R$" + precoFinal;
+	}
 
 }
