891d085dc121e0ce3fc3b002613e2e1327474b0c
gabriel7mendes <andrewsseven@gmail.com>
2022-05-17 09:53:20 -0300

    340. Novo Produto

---
+++ exercicios-jpa/src/modelo/basico/NovoProduto.java
@@ -0,0 +1,19 @@
+package modelo.basico;
+
+import infra.DAO;
+
+public class NovoProduto {
+	
+	public static void main(String[] args) {
+		//Produto produto = new Produto("Caneta", 7.45);
+		//Produto produto = new Produto("Notebook", 2987.78);
+		Produto produto = new Produto("Monitor 23", 789.98);
+		
+		DAO<Produto> dao = new DAO<>(Produto.class);
+		//dao.abrirT().incluir(produto).fecharT().fechar();
+		dao.incluirAtomico(produto).fechar();
+		
+		System.out.println("ID do produto: " + produto.getId());
+	}
+	
+}
